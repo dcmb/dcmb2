64ab35b86ca16f768ed745ca32354a4fcb941a4e
Andrew Bloomgarden <stalkingtiger@gmail.com>
2014-05-15 22:58:41 -0700

    Working code and assets for DCMB.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def cms_inheritable_block_content(identifier, blockable = @cms_page)
+    content = cms_block_content(identifier, blockable)
+    return cms_inheritable_block_content(identifier, blockable.parent) if content.blank? && blockable.parent
+    content
+  end
 end
