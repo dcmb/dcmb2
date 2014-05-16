081c013e8ae1060633768e02587d097b6be1cbeb
Andrew Bloomgarden <stalkingtiger@gmail.com>
2014-05-15 23:13:42 -0700

    Heroku try 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  config.logger = ActiveSupport::TaggedLogging.new(STDOUT.new)
+  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
